bb56de07c4be7db8255ecb7be3c3971d40c5ac2b
jfossard29 <e22108366@univ-brest.fr>
2025-03-12 14:11:13 +0100

    Mise sur écoute sur le port 3000

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOrigins(List.of("http://localhost:5173"));
+                    corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
                     corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     corsConfig.setAllowCredentials(true);
